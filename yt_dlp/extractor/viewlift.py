b9ce067b7fb385a9451a645800a54554ebca0dab
W∆L13R <76449145+ritwik-ritnov@users.noreply.github.com>
2021-12-10 23:22:54 +0530

    Update viewlift.py

--- yt_dlp/extractor/viewlift.py
+++ yt_dlp/extractor/viewlift.py
@@ -21,7 +21,7 @@ class ViewLiftBaseIE(InfoExtractor):
         'funnyforfree': 'snagfilms',
         'hoichoi': 'hoichoitv',
         'chorki' : 'prothomalo',
-        'aha' : 'aha'
+        'aha' : 'aha',
         'kiddovid': 'snagfilms',
         'laxsportsnetwork': 'lax',
         'legapallacanestro': 'lnp',
